6b037c56e74615e403298cab68a6414bbfd9cc4d
Serverket <serverket@mailfence.com>
2023-01-04 21:13:29 -0400

    ButtonUp position fix.

--- src/utils/ButtonUp.jsx
+++ src/utils/ButtonUp.jsx
@@ -14,7 +14,7 @@ export default function ButtonUp() {
       {offset > 140 && (
         <a
           href="#"
-          className="fixed bottom-4 right-2 bg-[#00a6ffff] text-white rounded-full w-10 h-10 flex items-center justify-center text-xl md:text-xl hover:bg-gray-100 filter hover:drop-shadow-md z-30 hover:text-[#00a6ffff] transition ease-in-out duration-200"
+          className="fixed bottom-4 right-2 bg-[#00a6ffff] text-white rounded-full w-10 h-10 flex items-center justify-center text-xl md:text-xl hover:bg-gray-100 filter hover:drop-shadow-md hover:text-[#00a6ffff] transition ease-in-out duration-200 z-40"
         >
           <i className="icon-up-open-2 text-2xl filter drop-shadow-md z-50" />
         </a>
