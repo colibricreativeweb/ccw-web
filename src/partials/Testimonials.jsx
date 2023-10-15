71b953fa74d8a276f5aff345a51b4a49b48fbf31
Serverket <serverket@mailfence.com>
2023-10-15 10:13:41 -0400

    Team section updated with pictures, text and links.

--- src/partials/Testimonials.jsx
+++ src/partials/Testimonials.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 import TestimonialImage1 from "../images/clients/biomelifescan.webp";
 import jaime from "../images/clients/jaime.webp";
@@ -51,14 +51,17 @@ function Testimonials() {
             <h2 className="h2 mb-4">
               We're{" "}
               <span className="underline underline-offset-4 decoration-blue-400">
-                Verified 
+                Verified
               </span>
-              <span className="md:underline md:underline-offset-4 md:decoration-blue-400"> Professionals</span>{" "}
+              <span className="md:underline md:underline-offset-4 md:decoration-blue-400">
+                {" "}
+                Professionals
+              </span>{" "}
               trusted by companies all over the world
             </h2>
             <p className="text-xl text-gray-600 -mb-8" data-aos="zoom-y-out">
-              We're a great organization because our team members are that good, we
-              combine a select group of people with skilled and talented
+              We're a great organization because our team members are that good,
+              we combine a select group of people with skilled and talented
               crafting showcased on their public profiles on
             </p>
           </div>
@@ -170,7 +173,10 @@ function Testimonials() {
                   >
                     <div className="relative flex items-start rounded bg-white">
                       {/* Testimonial */}
-                      <div className="text-center px-12 py-8 pt-20 mx-4 md:mx-0" id="testimonials">
+                      <div
+                        className="text-center px-12 py-8 pt-20 mx-4 md:mx-0"
+                        id="testimonials"
+                      >
                         <div className="absolute top-0 -mt-8 left-1/2 transform -translate-x-1/2">
                           <svg
                             className="absolute top-0 right-0 -mt-3 -mr-8 w-16 h-16 fill-current text-blue-500"
@@ -243,6 +249,10 @@ function Testimonials() {
         </div>
       </div>
 
+      {/* Section header */}
+      <div className="max-w-3xl mx-auto text-center pb-8 md:pb-14">
+        <h2 className="h2 mb-4">Meet The Team</h2>
+      </div>
       {/* Team */}
       <div
         className="md:grid md:grid-cols-3 lg:grid-cols-3 2xl:flex items-center md:items-start justify-center space-x-0 md:flex-row flex-col mb-20"
@@ -407,7 +417,7 @@ function Testimonials() {
                 Roberto Perez
               </h3>
               <p className="text-center group-hover:text-gray-50 dark:group-hover:text-gray-800 relative">
-                <strong>CTO</strong> | Back-end Dev
+                <strong>CTO</strong> | WordPress Full Stack Dev
               </p>
             </header>
 
@@ -415,6 +425,16 @@ function Testimonials() {
               className="flex justify-center space-x-4 mt-16 text-[#00a6ffff]
                    relative"
             >
+              <li className="motion-safe:translate-y-[100px] group-hover:translate-y-0 motion-safe:delay-100 motion-safe:transition">
+                <a
+                  href="https://robertperezc.com"
+                  className="pointer-cursor"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="icon-link-2 text-md hover:underline hover:underline-offset-4" />
+                </a>
+              </li>
               <li className="motion-safe:translate-y-[100px] group-hover:translate-y-0 motion-safe:delay-100 motion-safe:transition">
                 <a
                   href="https://github.com/R3is3rsf/"
@@ -438,6 +458,124 @@ function Testimonials() {
               <li className="motion-safe:translate-y-[100px] group-hover:translate-y-0 motion-safe:delay-300 motion-safe:transition">
                 <a
                   href="https://www.linkedin.com/in/r3is3rsf/"
+                  className="pointer-cursor"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="icon-linkedin text-md hover:underline hover:underline-offset-4" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div x-data="lw" className="flex justify-center items-start">
+          <div
+            className="w-full max-w-[300px] p-8 rounded-xl text-gray-800 dark:text-gray-400 overflow-hidden group 
+                    hover:shadow-2xl hover:shadow-[#00a6ffff]/50 motion-safe:transition-all motion-safe:duration-700"
+          >
+            <figure
+              className="relative w-40 h-40 md:w-36 md:h-36 m-0 mx-auto rounded-full outline outline-offset-4 outline-[#00a6ffff] 
+                       before:content-[''] before:absolute before:block before:pointer-events-none before:rounded-full before:h-full before:w-full before:bg-[#00a6ffff] before:-z-[1]
+                       group-hover:before:scale-[2.7] motion-safe:before:transition-all 
+                       motion-safe:transform-gpu motion-safe:before:duration-500 before:origin-center group-hover:outline-sky-400"
+            >
+              <img
+                className="rounded-full block max-w-full bg-[#00a6ffff] object-cover z-10 relative"
+                src="/team/karen.webp"
+                alt="Avatar"
+              />
+            </figure>
+            <header className="motion-safe:translate-y-2 group-hover:translate-y-0 motion-safe:transition-transform motion-safe:transform-gpu motion-safe:duration-500">
+              <h3 className="font-semibold text-xl text-center text-[#00a6ffff] mt-6 group-hover:text-gray-50 dark:group-hover:text-gray-800 relative">
+                Karen Barillas
+              </h3>
+              <p className="text-center group-hover:text-gray-50 dark:group-hover:text-gray-800 relative">
+                <strong>CDO</strong> | Graphic Designer
+              </p>
+            </header>
+
+            <ul
+              className="flex justify-center space-x-4 mt-16 text-[#00a6ffff]
+                   relative"
+            >
+              <li className="motion-safe:translate-y-[100px] group-hover:translate-y-0 motion-safe:delay-100 motion-safe:transition">
+                <a
+                  href="https://www.instagram.com/kbbmdesign"
+                  className="pointer-cursor"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="icon-instagram text-md hover:underline hover:underline-offset-4" />
+                </a>
+              </li>
+              <li className="motion-safe:translate-y-[100px] group-hover:translate-y-0 motion-safe:delay-200 motion-safe:transition">
+                <a
+                  href="https://www.behance.net/karenbarillas2"
+                  className="pointer-cursor"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="icon-behance text-md hover:underline hover:underline-offset-4" />
+                </a>
+              </li>
+              <li className="motion-safe:translate-y-[100px] group-hover:translate-y-0 motion-safe:delay-300 motion-safe:transition">
+                <a
+                  href="https://www.linkedin.com/mwlite/in/karen-barillas-466272162"
+                  className="pointer-cursor"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="icon-linkedin text-md hover:underline hover:underline-offset-4" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div x-data="lw" className="flex justify-center items-start">
+          <div
+            className="w-full max-w-[300px] p-8 rounded-xl text-gray-800 dark:text-gray-400 overflow-hidden group 
+                    hover:shadow-2xl hover:shadow-[#00a6ffff]/50 motion-safe:transition-all motion-safe:duration-700"
+          >
+            <figure
+              className="relative w-40 h-40 md:w-36 md:h-36 m-0 mx-auto rounded-full outline outline-offset-4 outline-[#00a6ffff] 
+                       before:content-[''] before:absolute before:block before:pointer-events-none before:rounded-full before:h-full before:w-full before:bg-[#00a6ffff] before:-z-[1]
+                       group-hover:before:scale-[2.7] motion-safe:before:transition-all 
+                       motion-safe:transform-gpu motion-safe:before:duration-500 before:origin-center group-hover:outline-sky-400"
+            >
+              <img
+                className="rounded-full block max-w-full bg-[#00a6ffff] object-cover z-10 relative"
+                src="/team/angie.webp"
+                alt="Avatar"
+              />
+            </figure>
+            <header className="motion-safe:translate-y-2 group-hover:translate-y-0 motion-safe:transition-transform motion-safe:transform-gpu motion-safe:duration-500">
+              <h3 className="font-semibold text-xl text-center text-[#00a6ffff] mt-6 group-hover:text-gray-50 dark:group-hover:text-gray-800 relative">
+                Oriangie Gonzalez
+              </h3>
+              <p className="text-center group-hover:text-gray-50 dark:group-hover:text-gray-800 relative">
+                <strong>Lead Designer</strong> | UX Researcher
+              </p>
+            </header>
+
+            <ul
+              className="flex justify-center space-x-4 mt-16 text-[#00a6ffff]
+                   relative"
+            >
+              <li className="motion-safe:translate-y-[100px] group-hover:translate-y-0 motion-safe:delay-200 motion-safe:transition">
+                <a
+                  href="https://www.behance.net/oriangigonzale"
+                  className="pointer-cursor"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="icon-behance text-md hover:underline hover:underline-offset-4" />
+                </a>
+              </li>
+              <li className="motion-safe:translate-y-[100px] group-hover:translate-y-0 motion-safe:delay-300 motion-safe:transition">
+                <a
+                  href="https://www.linkedin.com/in/oriangie-gonz%C3%A1lez-12b534231"
                   className="pointer-cursor"
                   target="_blank"
                   rel="noopener noreferrer"
@@ -586,70 +724,6 @@ function Testimonials() {
               <li className="motion-safe:translate-y-[100px] group-hover:translate-y-0 motion-safe:delay-300 motion-safe:transition">
                 <a
                   href="https://linkedin.com/in/jefferson-bustamante-hurtado-844218169"
-                  className="pointer-cursor"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="icon-linkedin text-md hover:underline hover:underline-offset-4" />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div x-data="lw" className="flex justify-center items-start">
-          <div
-            className="w-full max-w-[300px] p-8 rounded-xl text-gray-800 dark:text-gray-400 overflow-hidden group 
-                    hover:shadow-2xl hover:shadow-[#00a6ffff]/50 motion-safe:transition-all motion-safe:duration-700"
-          >
-            <figure
-              className="relative w-40 h-40 md:w-36 md:h-36 m-0 mx-auto rounded-full outline outline-offset-4 outline-[#00a6ffff] 
-                       before:content-[''] before:absolute before:block before:pointer-events-none before:rounded-full before:h-full before:w-full before:bg-[#00a6ffff] before:-z-[1]
-                       group-hover:before:scale-[2.7] motion-safe:before:transition-all 
-                       motion-safe:transform-gpu motion-safe:before:duration-500 before:origin-center group-hover:outline-sky-400"
-            >
-              <img
-                className="rounded-full block max-w-full bg-[#00a6ffff] object-cover z-10 relative"
-                src="/team/karen.webp"
-                alt="Avatar"
-              />
-            </figure>
-            <header className="motion-safe:translate-y-2 group-hover:translate-y-0 motion-safe:transition-transform motion-safe:transform-gpu motion-safe:duration-500">
-              <h3 className="font-semibold text-xl text-center text-[#00a6ffff] mt-6 group-hover:text-gray-50 dark:group-hover:text-gray-800 relative">
-                Karen Barillas
-              </h3>
-              <p className="text-center group-hover:text-gray-50 dark:group-hover:text-gray-800 relative">
-                <strong>CDO</strong> | Graphic Designer
-              </p>
-            </header>
-
-            <ul
-              className="flex justify-center space-x-4 mt-16 text-[#00a6ffff]
-                   relative"
-            >
-              <li className="motion-safe:translate-y-[100px] group-hover:translate-y-0 motion-safe:delay-100 motion-safe:transition">
-                <a
-                  href="https://www.instagram.com/kbbmdesign"
-                  className="pointer-cursor"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="icon-instagram text-md hover:underline hover:underline-offset-4" />
-                </a>
-              </li>
-              <li className="motion-safe:translate-y-[100px] group-hover:translate-y-0 motion-safe:delay-200 motion-safe:transition">
-                <a
-                  href="https://www.behance.net/karenbarillas2"
-                  className="pointer-cursor"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="icon-behance text-md hover:underline hover:underline-offset-4" />
-                </a>
-              </li>
-              <li className="motion-safe:translate-y-[100px] group-hover:translate-y-0 motion-safe:delay-300 motion-safe:transition">
-                <a
-                  href="https://www.linkedin.com/mwlite/in/karen-barillas-466272162"
                   className="pointer-cursor"
                   target="_blank"
                   rel="noopener noreferrer"
